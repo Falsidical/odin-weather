0acf162c2fe16f796d034d750f03223d1d719475
Falsidical <franport@outlook.com>
2025-05-01 05:36:09 +0000

    Add Loading Indicator

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const form = document.querySelector('form');
 const temp = document.querySelector('h1');
 const city = document.querySelector('h2');
 const country = document.querySelector('h3');
+const overlay = document.querySelector('.overlay');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -13,9 +14,11 @@ form.addEventListener('submit', (e) => {
 });
 
 async function displayWeather(location, units) {
+  overlay.style.display = 'grid';
   const { resolvedAddress, description, currentConditions } = await getWeatherData(location, units);
   temp.innerText = `${currentConditions.temp}°${units === 'metric' ? 'C' : 'F'}`;
   const address = resolvedAddress.split(',');
   city.innerText = address[0];
   country.innerText = address[address.length - 1];
+  overlay.style.display = 'none';
 }
